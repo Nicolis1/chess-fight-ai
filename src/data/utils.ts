0b09fa14a640a99581b8a16f50039da34189504c
Joseph Agnelli <joeyagnelli@gmail.com>
2024-04-04 16:39:09 -0700

    updating bots

---
+++ src/data/utils.ts
@@ -0,0 +1,146 @@
+import { Chess } from 'chess.js';
+import { BotData } from './features/activeCodeSlice';
+export const STARTER_CODE1 =
+	//comment for code formatting
+	`function getMove(position){
+	/* edit your code here, 
+	your result must be a string like "a3" that appears
+	in the list of position.moves() */
+	return position.moves()[0];
+}
+return getMove(position);`;
+
+export const STARTER_CODE2 =
+	//comment for code formatting
+	`function getMove(position){
+	/* edit your code here, 
+	your result must be a string like "a3" that appears
+	in the list of position.moves() */
+	let moves = position.moves();
+
+
+	for(let move of moves){
+		position.move(move);
+		let isMate = position.isCheckmate();
+		position.undo();
+		if(isMate){
+			return move;
+		}
+	}
+	let move = moves[Math.floor(Math.random() * moves.length)];
+	return move;
+}
+return getMove(position);`;
+
+export function testBot(position) {
+	let moves = position.moves();
+	let move = moves[0];
+	return move;
+}
+
+type Game = {
+	playerColor:string,
+	board:Chess
+}
+type SimulatedGame = {
+	winner:boolean|null,
+	draw: boolean,
+	turns: number,
+	reachedMoveLimit:boolean,
+	moves:Array<string>,
+	playerColor:string
+}
+export async function simulateGames(decisionFunction, callback) {
+	const maxMoves = 50;
+	try {
+		const games:Array<Game> = [];
+		for (let i = 0; i < 10; i++) {
+			games.push({
+				playerColor: Math.random() < 0.5 ? 'w' : 'b',
+				board: new Chess(),
+			});
+		}
+		for (let i = 0; i < maxMoves * 2; i++) {
+			for (let game of games) {
+				// console.log(game.moveNumber());
+				if (game.board.moveNumber() < 100 && !game.board.isGameOver()) {
+					if (game.board.turn() === game.playerColor) {
+						game.board.move(decisionFunction(game.board));
+					} else {
+						game.board.move(testBot(game.board));
+					}
+				}
+			}
+		}
+		const results:Array<SimulatedGame> = [];
+		for (let game of games) {
+			let winner:boolean|null = null;
+			if (game.board.isCheckmate()) {
+				// the winner is whoever's turn it isn't on the last turn
+				winner = game.board.turn() !== game.playerColor;
+				console.log(game.playerColor);
+				console.log(winner);
+			}
+			results.push({
+				winner,
+				draw: game.board.isDraw(),
+				turns: game.board.moveNumber(),
+				reachedMoveLimit: game.board.moveNumber() > maxMoves,
+				moves: game.board.history(),
+				playerColor: game.playerColor,
+			});
+		}
+		callback({ success: true, opponent: 'TestBot', results });
+	} catch (error) {
+		console.error(error);
+		callback({ success: false, error });
+	}
+}
+
+export async function fetchBots() {
+	const response = await fetch('/bots/all', {
+		method: 'GET',
+		cache: 'no-cache',
+	});
+	const jsonBots = JSON.parse(await response.json()).bots;
+	let botsForState: Array<BotData> = [];
+
+	for (let bot of jsonBots) {
+		botsForState.push({
+			id: bot.botid,
+			name: bot.name,
+			code: bot.code,
+		});
+	}
+	return botsForState;
+}
+
+export async function newBot():Promise<BotData | null>{
+	try {
+					
+		const resp = await fetch(`/bots/new`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ code: STARTER_CODE2 }),
+		})
+		
+		console.log(resp);
+		const response = JSON.parse(await resp.json());
+			// fetch the bots again so cache is ready for when you next open the drawer
+		fetch('/bots/all', {
+				method: 'GET',
+				cache: 'reload',
+		});
+		return {
+			id: response.bot_id,
+			name: response.name,
+			code: response.code,
+		}
+
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
+}
