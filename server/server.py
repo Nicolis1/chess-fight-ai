0a25a2aad03bdca654efc6db5473f6c4bf94438c
Joseph Agnelli <joeyagnelli@gmail.com>
2024-04-23 15:09:07 -0700

    add host and port to app.run

--- server/server.py
+++ server/server.py
@@ -570,4 +570,6 @@ def parse_json(data):
     return jsonify(json_util.dumps(data))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host=host, port=port)
